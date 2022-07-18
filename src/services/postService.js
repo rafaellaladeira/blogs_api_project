add375c0fb6cf1f410bb10d6841e6aec24e7ecc8
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-07-18 14:59:48 -0300

    feat: deletando consoles logs

--- src/services/postService.js
+++ src/services/postService.js
@@ -1,5 +1,4 @@
 const { Op } = require('sequelize');
-const Sequelize = require('sequelize');
 const { sequelize } = require('../database/models');
 const db = require('../database/models');
 const errorArray = require('../helpers/error');
@@ -7,7 +6,6 @@ const errorArray = require('../helpers/error');
 const postRouter = async ({ getEmail, allData }) => {
     const { title, content, categoryIds } = allData;
     const data = await db.User.findOne({ where: { email: getEmail } });
-    console.log('ANALISE DE ID PRA POSTAR', data);
     const result = await db.BlogPost.create({ title, content, userId: data.dataValues.id });
     await Promise.all(categoryIds.map(async (id) => {
         await db.PostCategory.create({ postId: result.null, categoryId: id });
@@ -75,8 +73,7 @@ const deletePost = async ({ id, email }) => {
     const result = await db.BlogPost.findByPk(id, {
         attributes: ['userId'] });
     if (result.dataValues.userId !== getUserId.dataValues.id) throw errorArray[12];
-    console.log('comparação', result.dataValues.userId, getUserId.dataValues.id);
-    
+       
     const t = await sequelize.transaction();
    
     const data = await db.BlogPost.destroy(
@@ -87,7 +84,7 @@ const deletePost = async ({ id, email }) => {
                 { where: { postId: id } },
                 { transaction: t },
                 );
-                console.log('DATAAAAAA', data);
+              
         return data;
 };
 
